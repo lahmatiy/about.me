6443842922215d7388e4eb8a2fafa2cc3ff68828
Roman Dvornov <rdvornov@gmail.com>
2019-09-15 01:01:40 +0300

    Set favicon

--- .discoveryrc.js
+++ .discoveryrc.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'Roman Dvornov',
     data: () => require('./data.json'),
     viewport: 'width=device-width, initial-scale=1',
+    favicon: __dirname + '/rdvornov.jpg',
     view: {
         assets: [
             'view.js',
