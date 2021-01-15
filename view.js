a8a08ad2d798e9fbd4c0c72899408ab3be86e6f9
Roman Dvornov <rdvornov@gmail.com>
2021-01-15 04:14:33 +0100

    Add non geek mode and fixes in data

--- view.js
+++ view.js
@@ -1,8 +1,152 @@
 discovery.page.define('default', {
-    view: 'struct',
-    expanded: 2,
-    limit: 12
-});
+    view: 'context',
+    modifiers: {
+        view: 'toggle-group',
+        name: 'mode',
+        data: [
+            {
+                value: 'oldschool',
+                text: 'For geeks'
+            },
+            {
+                value: 'forhumans',
+                text: 'For others'
+            }
+        ]
+    },
+    content: {
+        view: 'switch',
+        content: [
+            { when: '#.mode="forhumans"', content: {
+                view: 'block',
+                className: 'for-humans',
+                content: [
+                    'h1:who.name',
+                    'md:who.bio',
+                    {
+                        view: 'inline-list',
+                        data: 'who.entries().[value ~= /^http/]',
+                        item: 'badge:{ text: key, href: value, external: true }'
+                    },
+
+                    'h2:"Open Source Projects"',
+                    {
+                        view: 'ol',
+                        className: 'projects',
+                        limit: false,
+                        data: 'projects',
+                        item: [
+                            'link:{ text: title, href: url, external: true }',
+                            {
+                                view: 'context',
+                                when: 'role',
+                                content: [
+                                    'badge:{ text: role, color: "#dcaf025c" }'
+                                ]
+                            },
+                            'text:" – " + description',
+                            {
+                                view: 'ul',
+                                data: 'satellites',
+                                whenData: true,
+                                item: [
+                                    'link:{ text: title, href: url, external: true }',
+                                    {
+                                        view: 'context',
+                                        when: 'role',
+                                        content: [
+                                            'badge:{ text: role, color: "#dcaf025c" }'
+                                        ]
+                                    },
+                                    'text:" – " + description'
+                                ]
+                            }
+                        ]
+                    },
+
+                    'h2:"Talks"',
+                    {
+                        view: 'ol',
+                        className: 'talks',
+                        limit: false,
+                        data: 'talks',
+                        item: [
+                            {
+                                view: 'switch',
+                                content: [
+                                    { when: 'slides', content: 'link:{ text: title, href: slides, external: true }' },
+                                    { content: 'text:title' }
+                                ]
+                            },
+                            'text:" "',
+                            {
+                                view: 'badge',
+                                when: 'slides_en',
+                                data: '{ text: "Slides in English", href: slides_en, external: true, color: "#dcaf025c" }'
+                            },
+                            {
+                                view: 'badge',
+                                when: 'video',
+                                data: '{ text: "Video", href: video, external: true, color: "#dc02025c" }'
+                            },
+                            {
+                                view: 'block',
+                                className: 'talk-details',
+                                content: 'text:`${date} / ${event} / ${location}`'
+                            }
+                        ]
+                    },
+
+                    'h2:"Articles"',
+                    {
+                        view: 'ol',
+                        limit: false,
+                        data: 'articles',
+                        item: [
+                            'link:{ text: title, href: url, external: true }',
+                            'text:` (${date})`'
+                        ]
+                    },
+
+                    'h2:"Participation in podcasts"',
+                    {
+                        view: 'ol',
+                        className: 'shows',
+                        limit: false,
+                        data: 'shows',
+                        item: [
+                            'text:title',
+                            {
+                                view: 'ul',
+                                data: 'episodes',
+                                item: [
+                                    {
+                                        view: 'switch',
+                                        content: [
+                                            { when: 'url', content: 'link:{ text: title or num, href: url, external: true }' },
+                                            { content: 'text:title or num' }
+                                        ]
+                                    },
+                                    {
+                                        view: 'inline-list',
+                                        when: 'media',
+                                        data: 'media.entries()',
+                                        item: 'badge:{ text: key, href: value, external: true }'
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            } },
+            { content: {
+                view: 'struct',
+                expanded: 2,
+                limit: 12
+            } }
+        ]
+    }
+ });
 
 discovery.view.define('video', (el, config, data) => {
     const { video } = typeof data === 'string' ? { video: data } : data;
